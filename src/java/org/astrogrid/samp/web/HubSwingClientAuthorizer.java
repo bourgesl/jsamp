d863d9af7e1528896d741a3450dc75fb529c5e03
bourgesl <bourges.laurent@gmail.com>
2014-07-04 17:19:38 +0200

    merged with async-call branch

--- src/java/org/astrogrid/samp/web/HubSwingClientAuthorizer.java
+++ src/java/org/astrogrid/samp/web/HubSwingClientAuthorizer.java
@@ -1,19 +1,31 @@
 package org.astrogrid.samp.web;
 
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.GraphicsEnvironment;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.HeadlessException;
+import java.awt.Insets;
 import java.awt.Window;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import org.astrogrid.samp.httpd.HttpServer;
 
 /**
@@ -26,6 +38,7 @@ import org.astrogrid.samp.httpd.HttpServer;
 public class HubSwingClientAuthorizer implements ClientAuthorizer {
 
     private final Component parent_;
+    private static final int MAX_POPUP_WIDTH = 500;
     private static final Logger logger_ =
         Logger.getLogger( HubSwingClientAuthorizer.class.getName() );
 
@@ -49,7 +62,7 @@ public class HubSwingClientAuthorizer implements ClientAuthorizer {
             AuthResourceBundle
            .getAuthContent( ResourceBundle
                            .getBundle( AuthResourceBundle.class.getName() ) );
-        String[] qmsg = getMessageLines( request, appName, authContent );
+        Object[] qmsg = getMessageLines( request, appName, authContent );
         String noOpt = authContent.noWord();
         String yesOpt = authContent.yesWord();
 
@@ -74,17 +87,17 @@ public class HubSwingClientAuthorizer implements ClientAuthorizer {
     }
 
     /**
-     * Returns some lines of text describing the applying client.
-     * Suitable for directing to the user.
+     * Returns a "message" object describing the applying client to the user.
+     * The return value is suitable for use as the <code>msg</code> argument
+     * of one of <code>JOptionPane</code>'s methods.
      * 
      * @param  request  HTTP request bearing the application
      * @param   appName  application name claimed by the applicant
      * @param   authContent  content of AuthResourceBundle bundle
-     * @return   array of lines of plain text suitable for describing the
-     *           applicant to the user; not too long; each element
-     *           represents a screen line
+     * @return   message array describing the applicant to the user
+     * @see   javax.swing.JOptionPane
      */
-    private String[] getMessageLines( HttpServer.Request request,
+    private Object[] getMessageLines( HttpServer.Request request,
                                       String appName,
                                       AuthResourceBundle.Content authContent ) {
         Map headerMap = request.getHeaderMap();
@@ -100,13 +113,12 @@ public class HubSwingClientAuthorizer implements ClientAuthorizer {
         List lineList = new ArrayList();
         lineList.addAll( toLineList( authContent.appIntroductionLines() ) );
         lineList.add( "\n" );
-        lineList.add( "    " + authContent.nameWord() + ": " + appName );
-        lineList.add( "    " + authContent.originWord() + ": "
-                    + ( origin == null ? authContent.undeclaredWord()
-                                       : origin ) );
-        lineList.add( "    " + "URL" + ": "
-                    + ( referer == null ? authContent.undeclaredWord()
-                                        : referer ) );
+        Map infoMap = new LinkedHashMap();
+        infoMap.put( authContent.nameWord(), appName );
+        infoMap.put( authContent.originWord(), origin );
+        infoMap.put( "URL", referer );
+        lineList.add( createLabelledFields( infoMap,
+                                            authContent.undeclaredWord() ) );
         lineList.add( "\n" );
         if ( referer != null && origin != null &&
              ! origin.equals( getOrigin( referer ) ) ) {
@@ -122,7 +134,60 @@ public class HubSwingClientAuthorizer implements ClientAuthorizer {
         lineList.addAll( toLineList( authContent.adviceLines() ) );
         lineList.add( "\n" );
         lineList.add( authContent.questionLine() );
-        return (String[]) lineList.toArray( new String[ 0 ] );
+        return lineList.toArray();
+    }
+
+    /**
+     * Returns a component displaying name/value pairs represented by
+     * a given String->String map.
+     *
+     * @param  infoMap  String->String map of key->value pairs
+     * @param  undeclaredWord  text to use to indicate a null value
+     * @return   display component
+     */
+    private JComponent createLabelledFields( Map infoMap,
+                                             String undeclaredWord ) {
+        GridBagLayout layer = new GridBagLayout();
+        JComponent box = new JPanel( layer ) {
+            public Dimension getPreferredSize() {
+                Dimension size = super.getPreferredSize();
+                return new Dimension( Math.min( size.width, MAX_POPUP_WIDTH ),
+                                      size.height );
+            }
+        };
+        GridBagConstraints keyCons = new GridBagConstraints();
+        GridBagConstraints valCons = new GridBagConstraints();
+        keyCons.gridy = 0;
+        valCons.gridy = 0;
+        keyCons.gridx = 0;
+        valCons.gridx = 1;
+        keyCons.anchor = GridBagConstraints.WEST;
+        valCons.anchor = GridBagConstraints.WEST;
+        keyCons.fill = GridBagConstraints.NONE;
+        valCons.fill = GridBagConstraints.HORIZONTAL;
+        keyCons.weighty = 1;
+        valCons.weighty = 1;
+        keyCons.weightx = 0;
+        valCons.weightx = 1;
+        valCons.insets = new Insets( 1, 1, 1, 1 );
+
+        JComponent stack = Box.createVerticalBox();
+        for ( Iterator it = infoMap.keySet().iterator(); it.hasNext(); ) {
+            String key = (String) it.next();
+            String value = (String) infoMap.get( key );
+            String valtxt = value == null ? undeclaredWord : value;
+            JComponent keyComp = new JLabel( key + ": " );
+            JTextField valueField = new JTextField( valtxt );
+            valueField.setEditable( false );
+            layer.setConstraints( keyComp, keyCons );
+            layer.setConstraints( valueField, valCons );
+            box.add( keyComp );
+            box.add( valueField );
+            keyCons.gridy++;
+            valCons.gridy++;
+        }
+        box.setBorder( BorderFactory.createEmptyBorder( 0, 0, 0, 0 ) );
+        return box;
     }
 
     /**
